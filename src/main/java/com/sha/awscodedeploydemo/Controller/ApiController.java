97fb67f8b3b27a746eafed6bcae2f0b6f6b77b06
Renata-CDB <renatabianchine2016@gmail.com>
2022-03-24 17:49:41 -0300

    first commit

---
+++ src/main/java/com/sha/awscodedeploydemo/Controller/ApiController.java
@@ -0,0 +1,15 @@
+package com.sha.awscodedeploydemo.Controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController
+{
+    @GetMapping("api/health")
+     public ResponseEntity<?> healthCheck()
+     {
+         return ResponseEntity.ok("It works successfully.");
+     }
+}
